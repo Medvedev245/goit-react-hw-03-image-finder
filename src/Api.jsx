b238b1158571b0a7432fe268e25a205f83de302f
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-10-17 17:01:47 +0200

    add

--- src/Api.jsx
+++ src/Api.jsx
@@ -6,16 +6,21 @@ axios.defaults.baseURL = `https://pixabay.com/api/`;
 export const fetchImages = async (query, page) => {
   const separated = query.split('/');
   const exstractedQuery = separated[1];
-
-  const response = await axios.get('', {
-    params: {
-      key: API_KEY,
-      q: exstractedQuery,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      page: page,
-      per_page: 12,
-    },
-  });
-  return response.data.hits;
+  try {
+    const response = await axios.get('', {
+      params: {
+        key: API_KEY,
+        q: exstractedQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        page: page,
+        per_page: 12,
+      },
+    });
+    console.log(response.data.hits);
+    return response.data.hits;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { Toaster } from 'react-hot-toast';
 import { Searchbar } from './Searchbar/Searchbar';
 import { Container } from './App.styled';
 import { fetchImages } from '../Api';
+import { Loader } from './Loader/Loader';
 import { notifyInfo, notifyInputQuerry, success } from './Notify/Notify';
 
 export class App extends Component {
@@ -11,6 +12,17 @@ export class App extends Component {
     images: [],
     page: 1,
     loading: false,
+  };
+
+  componentDidUpdate = async (prevProps, prevState) => {
+    const prevQuery = prevState.query;
+    const searchQuery = this.state.query;
+    const prevPage = prevState.page;
+    const nexPage = this.state.page;
+
+    if (prevQuery !== searchQuery || prevPage !== nexPage) {
+      this.loadResult();
+    }
   };
 
   changeQuery = newQuery => {
@@ -28,10 +40,12 @@ export class App extends Component {
     try {
       this.setState({ loading: true });
       const img = await fetchImages(searchQuery, nexPage);
+      console.log(img);
       if (img.length) {
         this.setState(prevState => ({
           images: this.state.page > 1 ? [...prevState.images, ...img] : img,
         }));
+        console.log(img);
         success(searchQuery);
         this.setState({ loading: false });
       } else {
@@ -42,6 +56,12 @@ export class App extends Component {
       console.log(error);
       this.setState({ loading: false });
     }
+  };
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
   };
 
   handleSubmit = evt => {
@@ -55,9 +75,12 @@ export class App extends Component {
   };
 
   render() {
+    const { loading, images } = this.state;
     return (
       <Container>
         <Searchbar onSubmit={this.handleSubmit} />
+        {/* { loading && <Loader /> } */}
+        { images.length > 0 && <Gallery imgItems={ images } /> } 
         <Toaster position="top-center" reverseOrder={true} />
       </Container>
     );
