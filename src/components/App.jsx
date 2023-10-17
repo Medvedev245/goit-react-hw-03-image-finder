1835b21e7f1e3837eac7568bb8f2819afd3c15ac
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-10-17 12:02:16 +0200

    йцу

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Container } from './App.styled';
+import { fetchImages } from '../Api';
+import { notifyInfo, notifyInputQuerry, success } from './Notify/Notify';
+
+export class App extends Component {
+  state = {
+    query: '',
+    images: [],
+    page: 1,
+    loading: false,
+  };
+
+  changeQuery = newQuery => {
+    this.setState({
+      query: `${Date.now()}/${newQuery}`,
+      images: [],
+      page: 1,
+    });
+  };
+
+  loadResult = async () => {
+    const searchQuery = this.state.query;
+    const nexPage = this.state.page;
+
+    try {
+      this.setState({ loading: true });
+      const img = await fetchImages(searchQuery, nexPage);
+      if (img.length) {
+        this.setState(prevState => ({
+          images: this.state.page > 1 ? [...prevState.images, ...img] : img,
+        }));
+        success(searchQuery);
+        this.setState({ loading: false });
+      } else {
+        notifyInfo();
+        this.setState({ loading: false });
+      }
+    } catch (error) {
+      console.log(error);
+      this.setState({ loading: false });
+    }
+  };
+
+  handleSubmit = evt => {
+    console.log(evt.query);
+    if (evt.query === '') {
+      notifyInputQuerry();
+      return;
+    }
+    console.log(evt);
+    this.changeQuery(evt.query);
+  };
+
+  render() {
+    return (
+      <Container>
+        <Searchbar onSubmit={this.handleSubmit} />
+        <Toaster position="top-center" reverseOrder={true} />
+      </Container>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,35 @@
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+import { Search } from './Searchbar.styled';
+
+const nameSchema = Yup.object().shape({
+  query: Yup.string()
+    .min(2, 'Too Short!')
+    .max(25, 'Too Long!')
+    .required('Required'),
+});
+
+export const Searchbar = props => {
+  // console.log(props);
+  return (
+    <Search>
+      <Formik
+        initialValues={{
+          query: '',
+        }}
+        validationSchema={nameSchema}
+        onSubmit={(values, actions) => {
+          props.onSubmit(values);
+          actions.resetForm();
+        }}
+      >
+        <Form>
+          <label htmlFor="query">Image</label>
+          <Field id="query" name="query" placeholder="Enter Image" />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </Search>
+  );
+};
